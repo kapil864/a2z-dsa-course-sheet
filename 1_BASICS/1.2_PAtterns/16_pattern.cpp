a2b07d11aa542d91332c08fc1be42585196d79b5
kapilTantuway <kapil.kumar.svp@gmail.com>
2022-08-24 17:07:50 +0530

    Completed all the pattern questions

---
+++ 1_BASICS/1.2_PAtterns/16_pattern.cpp
@@ -0,0 +1,26 @@
+/*
+
+Print pattern
+
+A
+BB
+CCC
+DDDD
+EEEEE
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=5;
+    int ch = 'A';
+    for( int i =0; i<n; i++){
+        for( int j =0; j<= i ; j++){
+            cout<<char(ch+i);
+        }
+        cout<<endl;
+    }
+    return 0;
+}
